eb4ecde93034c3f43f1c64de257cfad54276418d
Jack Wilson <wils3005@gmail.com>
2020-09-18 14:51:43 -0400

    Fri, 18 Sep 2020 18:51:43 GMT
     2 files changed, 23 insertions(+), 27 deletions(-)

--- packages/server/src/env.ts
+++ packages/server/src/env.ts
@@ -1,30 +1,30 @@
 import * as z from "zod";
 import fs from "fs";
+import pino from "pino";
 
-const { HOST, PORT, PUBLIC_PATH, TLS_CERT, TLS_KEY } = process.env;
-
-const cert = fs.readFileSync(String(TLS_CERT));
-const key = fs.readFileSync(String(TLS_KEY));
-
-const host = z
+const hostType = z
   .string()
-  .refine((s) => /^((?:[0-9]{1,3}\.){3}[0-9]{1,3}|localhost)$/.exec(s), {
-    message: `process.env.HOST = ${String(HOST)}`,
-  })
-  .parse(HOST);
+  .refine(
+    (s) => (/^((?:[0-9]{1,3}\.){3}[0-9]{1,3}|localhost)$/.exec(s) || [])[0]
+  );
 
-const port = z
-  .string()
-  .refine((s) => Number(s) > 1024 && Number(s) < 65535, {
-    message: `process.env.PORT = ${String(PORT)}`,
+const { HOST, PINO_OPTIONS, PORT, PUBLIC_PATH, CERT, KEY } = z
+  .object({
+    CERT: z.string(),
+    HOST: hostType,
+    KEY: z.string(),
+    PINO_OPTIONS: z.string(),
+    PORT: z.string().refine((s) => Number(s) > 1024 && Number(s) < 65535),
+    PUBLIC_PATH: z.string().refine((s) => fs.statSync(s)),
   })
-  .parse(PORT);
+  .nonstrict()
+  .parse(process.env);
 
-const relativeTo = z
-  .string()
-  .refine(() => fs.statSync(String(PUBLIC_PATH)), {
-    message: `process.env.PUBLIC_PATH = ${String(PUBLIC_PATH)}`,
-  })
-  .parse(PUBLIC_PATH);
+const cert = fs.readFileSync(CERT);
+const key = fs.readFileSync(KEY);
+const host = HOST;
+const loggerOptions = JSON.parse(PINO_OPTIONS) as pino.LoggerOptions;
+const port = Number(PORT);
+const relativeTo = PUBLIC_PATH;
 
-export { cert, key, host, port, relativeTo };
+export { cert, key, host, loggerOptions, port, relativeTo };
--- packages/server/src/plugins/logger.ts
+++ packages/server/src/plugins/logger.ts
@@ -1,9 +1,5 @@
-import * as z from "zod";
+import { loggerOptions } from "../env";
 import pino from "pino";
-
-const loggerOptions = z
-  .record(z.unknown())
-  .parse(JSON.parse(z.string().parse(process.env.PINO_OPTIONS)));
 
 const logger = pino(loggerOptions);
 
